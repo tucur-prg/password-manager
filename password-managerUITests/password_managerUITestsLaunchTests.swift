a706b96074873801ea2d7ef6c183290a2bbc917d
n-shirasaki <n-shirasaki@cladminnoMacBook-Pro-2.local>
2023-10-19 09:51:23 +0900

    initialize

---
+++ password-managerUITests/password_managerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  password_managerUITestsLaunchTests.swift
+//  password-managerUITests
+//
+//  Created by n-shirasaki on 2023/10/19.
+//
+
+import XCTest
+
+final class password_managerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
