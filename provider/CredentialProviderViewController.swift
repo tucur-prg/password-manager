f0b469862a675df918b97b43851ddc3f023fc286
n-shirasaki <n-shirasaki@cladminnoMacBook-Pro-2.local>
2023-10-21 18:00:28 +0900

    passkeyのgetで動作するようにした

--- provider/CredentialProviderViewController.swift
+++ provider/CredentialProviderViewController.swift
@@ -12,25 +12,145 @@ import CBORCoding
 
 class CredentialProviderViewController: ASCredentialProviderViewController {
 
+    // AutoFilの鍵マークをクリックした時に実行されるところ
     override func prepareCredentialList(for serviceIdentifiers: [ASCredentialServiceIdentifier]) {
-        // AutoFilの鍵マークをクリックした時に実行されるところ
+        NSLog("call: prepareCredentialList deprecated")
+        NSLog("Class: %@", serviceIdentifiers[0])
+        NSLog("URL: %@", serviceIdentifiers[0].identifier)
+    }
+
+    // 
+    override func prepareCredentialList(for serviceIdentifiers: [ASCredentialServiceIdentifier], requestParameters: ASPasskeyCredentialRequestParameters) {
+
         NSLog("call: prepareCredentialList")
         NSLog("Class: %@", serviceIdentifiers[0])
         NSLog("URL: %@", serviceIdentifiers[0].identifier)
     }
 
+    // AutoFilで ASCredentialIdentityStore に保存したユーザー名をクリックした時によばれる
     override func provideCredentialWithoutUserInteraction(for credentialIdentity: ASPasswordCredentialIdentity) {
-        NSLog("call: provideCredentialWithoutUserInteraction")
+        NSLog("call: provideCredentialWithoutUserInteraction deprecated")
         NSLog("Class: %@", credentialIdentity)
 
         let passwordCredential = ASPasswordCredential(user: "xxxx", password: "xxxx")
         self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
     }
 
+    // info.plist の ProvidesPasskeys = true
+    // AutoFilで ASCredentialIdentityStore に保存したユーザー名をクリックした時によばれる
+    // Sign in with your passkey? の画面で Continue ボタンを押した時に呼び出される
+    override func provideCredentialWithoutUserInteraction(for credentialRequest: ASCredentialRequest) {
+        NSLog("call: provideCredentialWithoutUserInteraction")
+        // ASCredentialRequest.type をみてハンドリング
+        if credentialRequest.type == .passkeyAssertion {
+            let passkeyRequest = credentialRequest as! ASPasskeyCredentialRequest
+            let passkeyCredentialIdentity = passkeyRequest.credentialIdentity as! ASPasskeyCredentialIdentity
+            
+            NSLog("ClientDataHash: \(passkeyRequest.clientDataHash.hexEncodedString())")
+            NSLog("UserVerification: \(passkeyRequest.userVerificationPreference.rawValue)")
+            for alg in passkeyRequest.supportedAlgorithms {
+                NSLog("Alg: \(alg.rawValue)")
+            }
+            
+            NSLog("User: \(passkeyCredentialIdentity.user)")
+            NSLog("UserName: \(passkeyCredentialIdentity.userName)")
+            NSLog("RelyingPartyIdentifier: \(passkeyCredentialIdentity.relyingPartyIdentifier)")
+            NSLog("Rank: \(passkeyCredentialIdentity.rank)")
+            NSLog("UserHandle: \(passkeyCredentialIdentity.userHandle.hexEncodedString())")
+            NSLog("CredentialID: \(passkeyCredentialIdentity.credentialID.hexEncodedString())")
+            
+            NSLog(passkeyCredentialIdentity.recordIdentifier ??  "<nil>")
+            
+            var authenticatorData = Data()
+            
+            let rpId = passkeyCredentialIdentity.relyingPartyIdentifier
+            let userHandle = passkeyCredentialIdentity.userHandle
+            
+            let rpIdHash = Data(SHA256.hash(data: rpId.data(using: .utf8)!))
+            let flags = Data([ UInt8(0x01 | 0x04 | 0x08 | 0x10) ])
+            let signCount = Data([0, 0, 0, 0])
+            let credentialId = passkeyCredentialIdentity.credentialID
+            
+            authenticatorData.append(rpIdHash)
+            authenticatorData.append(flags)
+            authenticatorData.append(signCount)
+            
+            var signature: Data?
+            do {
+                var error: Unmanaged<CFError>?
+                
+                guard let privateKey = load("sample") else {
+                    throw NSError(domain: "privateKey", code: -1, userInfo: nil)
+                }
+                
+                let message = authenticatorData + passkeyRequest.clientDataHash
+                guard let res = SecKeyCreateSignature(
+                    privateKey,
+                    .ecdsaSignatureMessageX962SHA256,
+                    message as CFData,
+                    &error) as Data?
+                else {
+                    throw NSError(domain: "sign error", code: -1, userInfo: nil)
+                }
+                
+                signature = res
+            } catch {
+                NSLog("error")
+            }
+            
+            NSLog(rpIdHash.hexEncodedString())
+            NSLog(flags.hexEncodedString())
+            NSLog(signCount.hexEncodedString())
+            NSLog(credentialId.hexEncodedString())
+            
+            NSLog(authenticatorData.hexEncodedString())
+            NSLog(authenticatorData.base64EncodedString())
+            
+            let passkeyCredential = ASPasskeyAssertionCredential(
+                userHandle: userHandle,
+                relyingParty: rpId,
+                signature: signature!,
+                clientDataHash: passkeyRequest.clientDataHash,
+                authenticatorData: authenticatorData,
+                credentialID: credentialId
+            )
+            
+            self.extensionContext.completeAssertionRequest(using: passkeyCredential) { success in
+                if success {
+                    NSLog("completeAssertionRequest success")
+                } else {
+                    NSLog("completeAssertionRequest failer")
+                }
+            }
+        } else {
+            let passwordRequest = credentialRequest as! ASPasswordCredentialRequest
+            let passwordCredentialIdentity = passwordRequest.credentialIdentity as! ASPasswordCredentialIdentity
+
+            NSLog("User: \(passwordCredentialIdentity.user)")
+            NSLog("Rank: \(passwordCredentialIdentity.rank)")
+            
+            NSLog(passwordCredentialIdentity.recordIdentifier ??  "<nil>")
+
+            let passwordCredential = ASPasswordCredential(user: "xxxx", password: "xxxx")
+            self.extensionContext.completeRequest(withSelectedCredential: passwordCredential) { success in
+                if success {
+                    NSLog("completeRequest success")
+                } else {
+                    NSLog("completeRequest failer")
+                }
+            }
+        }
+    }
+
+    override func prepareInterfaceToProvideCredential(for credentialRequest: ASPasswordCredentialIdentity) {
+        NSLog("call: prepareInterfaceToProvideCredential deprecated")
+    }
+
     override func prepareInterfaceToProvideCredential(for credentialRequest: ASCredentialRequest) {
         NSLog("call: prepareInterfaceToProvideCredential")
     }
 
+    // info.plist の ProvidesPasskeys = true
     // Create a passkey? の画面で Continue ボタンを押した時に呼び出される
     override func prepareInterface(forPasskeyRegistration registrationRequest: ASCredentialRequest) {
         NSLog("call: prepareInterface")
@@ -57,6 +177,9 @@ class CredentialProviderViewController: ASCredentialProviderViewController {
         var attestationObject: Data?
         
         let rpId = passkeyCredentialIdentity.relyingPartyIdentifier
+        let userName = passkeyCredentialIdentity.userName
+        let userHandle = passkeyCredentialIdentity.userHandle
+        
         let rpIdHash = Data(SHA256.hash(data: rpId.data(using: .utf8)!))
         let flags = Data([ UInt8(0x01 | 0x04 | 0x08 | 0x10 | 0x40 | 0x80) ])
         let signCount = Data([0, 0, 0, 0])
@@ -123,7 +246,7 @@ class CredentialProviderViewController: ASCredentialProviderViewController {
         NSLog(attestationObject!.base64EncodedString())
 
         let passkeyCredential = ASPasskeyRegistrationCredential(
-            relyingParty: passkeyCredentialIdentity.relyingPartyIdentifier,
+            relyingParty: rpId,
             clientDataHash: passkeyRequest.clientDataHash,
             credentialID: credentialId,
             attestationObject: attestationObject!
@@ -136,12 +259,35 @@ class CredentialProviderViewController: ASCredentialProviderViewController {
                 NSLog("completeRegistrationRequest failer")
             }
         }
+
+        // completeRegistrationRequest が failer のほうにいくので一旦ここに
+        let store = ASCredentialIdentityStore.shared;
+        store.getState {state in
+            if state.isEnabled {
+                // navigator.credentials.get の項目に表示されるようにする
+                let credential = ASPasskeyCredentialIdentity(
+                    relyingPartyIdentifier: rpId,
+                    userName: userName,
+                    credentialID: credentialId,
+                    userHandle: userHandle,
+                    recordIdentifier: nil)
+                    
+                store.saveCredentialIdentities([credential]) { bool, error in
+                    if let error = error {
+                        NSLog(error.localizedDescription)
+                    } else {
+                        NSLog("passkey save success")
+                    }
+                }
+            }
+        }
     }
 
+    // info.plist の ASCredentialProviderExtensionShowsConfigurationUI = true
+    // パスワードオプションのところで呼び出されるカスタムUI
     override func prepareInterfaceForExtensionConfiguration() {
         NSLog("call: prepareInterfaceForExtensionConfiguration")
 
-        // パスワードオプションのところで呼び出されるカスタムUI
         let sb = UIStoryboard(name: "FirstView", bundle: nil)
 
         let vc = sb.instantiateInitialViewController() as! FirstViewController
@@ -154,11 +300,6 @@ class CredentialProviderViewController: ASCredentialProviderViewController {
     override func viewDidLoad() {
         NSLog("call: viewDidLoad")
         super.viewDidLoad()
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        NSLog("call: viewDidAppear")
-        super.viewDidAppear(animated)
     }
 
     @IBAction func cancel(_ sender: AnyObject?) {
@@ -186,7 +327,7 @@ class CredentialProviderViewController: ASCredentialProviderViewController {
                     if let error = error {
                         NSLog(error.localizedDescription)
                     } else {
-                        NSLog("save success")
+                        NSLog("password save success")
                     }
                 }
             }
@@ -266,6 +407,13 @@ struct AttestationObject: Codable {
     var attStmt: [String: String]
     var authData: Data
 }
+
+struct AssrtsionObject: Codable {
+    var fmt: String
+    var attStmt: [String: String]
+    var authData: Data
+}
+
 
 struct CredentialPublicKeyEc: Codable {
     var typ: Int
